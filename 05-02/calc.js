55912599262c00e449302fb407ddc2a94f137831
Roberson <robersoncorreajr@gmail.com>
2025-02-05 15:58:38 -0300

    adicionando funcoes de calc

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -4,5 +4,15 @@
 let num1 = Number(prompt("Digite um número"));
 let num2 = Number(prompt("Digite outro número"));
 
+// Operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+
 // Imprimir resposta
-console.log(`A soma é ${num1 + num2}`);
+console.log(`A soma é ${soma}`);
+console.log(`A subtração é ${sub}`)
+console.log(`A multiplicação é ${mult}`)
+console.log(`A divisão é ${div}`)
