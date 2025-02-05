6a97abc3f14b582d45028aaad0bc0f954fd94690
Roberson <robersoncorreajr@gmail.com>
2025-02-05 14:54:51 -0300

    adicionando

---
+++ 05-02/script.js
@@ -0,0 +1,9 @@
+// Variavel
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos?");
+
+
+// Imprimir na tela
+document.write("Seja Bem-Vindo:  " + nome + ", Idade: " + idade);
+document.write("<br>");
+document.write(` Seja bem vindo: ${nome}, sua idade é: ${idade} anos`);
