77ca2ed5080a22aa045ad23a6cabd66da2c9652b
Roberson <robersoncorreajr@gmail.com>
2025-02-05 15:46:30 -0300

    adicionando um arquivo javascript calc

---
+++ 05-02/calc.js
@@ -0,0 +1,8 @@
+// Duas variáveis
+// Num1 e Num2
+// Pergunya para o usuario
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
+
+// Imprimir resposta
+console.log(`A soma é ${num1 + num2}`);
