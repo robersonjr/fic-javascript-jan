fe3babf1398b40f685660c25ced8c16852514de3
Roberson <robersoncorreajr@gmail.com>
2025-02-05 16:12:29 -0300

    adicionando

---
+++ 05-02/exer1.js
@@ -0,0 +1,9 @@
+/* 
+  -Armazene a palavra 'Tom' em uma let "tom";
+  -Armazene a palavra 'Hanks' em uma let "firstTom";
+  -Armazene a palavra 'Cruise' em uma let "secondTom";
+  -Declare uma let "myFavoriteTom" que recebe nome,
+    espaço, e sobrenome do Tom que você mais gosta;
+  -Exiba a "myFavoriteTom" no console do browser.  
+
+*/
