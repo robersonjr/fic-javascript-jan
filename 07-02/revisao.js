42d07b2f77ff537904538f1b319e6444bc299763
Roberson <robersoncorreajr@gmail.com>
2025-02-07 14:27:28 -0300

    conhecimentos de texto

---
+++ 07-02/revisao.js
@@ -0,0 +1,9 @@
+let mensagem ="Olá Roberson"; // String
+let idade = 37; // Int
+let casado = true; // Booleano
+let times = ["Paulista", "Santos", 12, true]
+
+console.log("Variavel mensagem", mensagem);
+console.log("Variavel idade", idade);
+console.log("Variavel casado", casado);
+console.log(times)
---
+++ 07-02/texto.js
@@ -0,0 +1,12 @@
+let text = "Estão chegando as provas";
+
+// Tamanho da string
+console.log(text.length);
+
+// Replace 
+console.log("Alterando", text.replace("provas", "Avaliações"));
+console.log(text)
+
+
+console.log(text.toLocaleLowerCase());
+console.log(text.toUpperCase())
