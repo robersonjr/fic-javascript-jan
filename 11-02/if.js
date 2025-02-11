ce5e67a5b1cb1a991e5c58036da7d305797b3989
Roberson <robersoncorreajr@gmail.com>
2025-02-11 13:56:10 -0300

    relembrando if

---
+++ 11-02/if.js
@@ -0,0 +1,12 @@
+/*
+   se a idade >=18, a pessoa pode ir para a festa 
+   senão , a pessoa não pode ir para festa 
+*/
+
+let idade =17;
+
+if (idade >= 18) {
+    console.log("A pessoa, pode ir para a festa");
+} else {
+    console.log("A pessoa, não pode ir para festa");
+}
