e75de5ec8924723f35b369da1b147fb7afede849
Roberson <robersoncorreajr@gmail.com>
2025-02-11 14:47:43 -0300

    nota do aluno

---
+++ 11-02/nota.js
@@ -0,0 +1,12 @@
+/*
+    se a nota do aluno for maior ou igual a 6, o aluno está aprovado
+    senão, o aluno esta aprovado 
+*/
+
+let nota = 6 ;
+
+if( nota >=6) {
+    console.log ("Aprovado");
+} else {
+    console.log ("Reprovado");
+}
