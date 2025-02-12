3d50571e503903e58aac94888378f194fce7671a
Roberson <robersoncorreajr@gmail.com>
2025-02-12 14:24:35 -0300

    adicionando mais funções no decremento

---
+++ 12-02/decremento.js
@@ -0,0 +1,15 @@
+//Subtração
+let valor = 7;
+
+valor = valor - 1;
+valor --;
+
+console.log(valor);
+
+//Multiplicação
+valor *= 2;
+console.log(valor);
+
+//Divisão
+valor /= 2;
+console.log(valor);
