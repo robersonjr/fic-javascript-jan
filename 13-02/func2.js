95d13f68316b39bfbd72d1da204f3e2b3cc1fbd5
Roberson <robersoncorreajr@gmail.com>
2025-02-13 16:21:13 -0300

    Executando exercicios de função

---
+++ 13-02/func2.js
@@ -0,0 +1,14 @@
+function mostrarNome (nome) {
+    document.write(nome);
+}
+
+function pularLinha() {
+    document.write("<br><br>");
+}
+
+pularLinha();
+mostrarNome("Fabio");
+pularLinha();
+mostrarNome("Caio");
+pularLinha();
+mostrarNome("Carla");
