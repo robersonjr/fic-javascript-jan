9f53822bce03ccd1b711ed60e49caec876f8487e
Roberson <robersoncorreajr@gmail.com>
2025-02-14 16:16:49 -0300

    Adicionando value no JS

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -5,4 +5,14 @@ const resultado = document.getElementById("resultado")
 function resultadoNotaClick(event) {
     event.preventDefault();
     console.log(inputNota.value);
+
+    //condição
+    if (inputNota.value >=6) {
+        resultado.innerHTML = "Aprovado,meus parabéns."
+    } else {
+        resultado.innerHTML = "Reprovado, melhore!"
+    }
+
+    inputNota.value ="";
+
 }
