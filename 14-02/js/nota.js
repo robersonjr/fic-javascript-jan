91e398768e83ecba0fd906135f1bfba4d685bd6a
Roberson <robersoncorreajr@gmail.com>
2025-02-18 14:37:23 -0300

    complementando html,css e js , relembrando conteudo aula passada

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -1,18 +1,28 @@
 // Variáveis
 const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("resultado")
+const message = document.getElementById("message")
 
 function resultadoNotaClick(event) {
     event.preventDefault();
     console.log(inputNota.value);
+
+    //Validação de formulário
+    if (inputNota.value === "") {
+        message.style.display="block";
+        //alert("Digite a nota");
+        return false;
+    }
 
     //condição
     if (inputNota.value >=6) {
         resultado.innerHTML = "Aprovado,meus parabéns."
     } else {
         resultado.innerHTML = "Reprovado, melhore!"
-    }
+    } 
 
+    message.style.display="none";
+  
     inputNota.value ="";
 
 }
