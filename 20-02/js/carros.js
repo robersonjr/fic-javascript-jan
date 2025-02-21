853b7a104f51215d301de46fe4831529ddec9f04
Roberson <robersoncorreajr@gmail.com>
2025-02-21 16:15:41 -0300

    complementando lista de carro

--- 20-02/js/carros.js
+++ 20-02/js/carros.js
@@ -9,10 +9,54 @@ const cars = [
         model: "Civic",
         year: 2008
     },
+    {
+        brand: "Nissan",
+        model: "Skyline GTR R-34",
+        year: 1998
+    },
+    {
+        brand: "Mitsubishi",
+        model: "Eclipse GST",
+        year: 1995
+    },
+    {
+        brand: "Mazda",
+        model: "RX-7",
+        year: 1997
+    },
 ];
 
-console.log(cars [0].model);
-//Ano do honda
-console.log(cars[1].year)
-//Ano do Toyota
-console.log(cars[0].year)
+/* Cria uma função e chama essa função displayCards, aqui mesmo */
+function displayCards () {
+    //Variavel
+    const cardList = document.querySelector("#card-list");
+    
+    cars.forEach((car) => {
+      // Criaos um elemento div pelo JS
+      const cardDiv = document.createElement("div");
+      //criamos uma class css car
+      cardDiv.classList.add('car');
+
+      //Criando o modelo
+      const carModel = document.createElement("h2");
+      carModel.textContent = `${car.brand} ${car.model}`;
+
+      //Criando o ano
+      const carYear = document.createElement("p");
+      carYear.textContent = `Ano : ${car.year}`;
+
+    
+    //Imprimindo na tela as informações
+    cardDiv.appendChild(carModel);
+    cardList.appendChild(cardDiv);
+    cardDiv.appendChild(carYear)
+
+
+    })
+
+};
+
+// Chamar função
+displayCards ();
+
+
