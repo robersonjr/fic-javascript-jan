fffdd695d6893bdbc5d976996a8d531c248e2759
Roberson <robersoncorreajr@gmail.com>
2025-02-21 14:08:28 -0300

    validando cadastro

--- 20-02/js/cadastro.js
+++ 20-02/js/cadastro.js
@@ -7,6 +7,12 @@ salvarBtn.addEventListener("click", function () {
     const valor = campo.value;
     //console.log(valor);
 
+       // Validação de formulario
+       if(campo.value=== ""){
+        alert("Preencha o campo!");
+        return false
+    }
+   
     // Salvar no localStorage
     const chave = `ValorSalvo_${Date.now()}`; //usa timestamppara chave única
     // Funcionalidade do localStorage
@@ -18,3 +24,4 @@ salvarBtn.addEventListener("click", function () {
     // Exibir valores salvos
     carregarValoresSalvos();
 });
+
