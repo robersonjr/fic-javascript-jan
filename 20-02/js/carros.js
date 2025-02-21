bf12000a6f7d6d41acdb50b61abfacd280f55e9d
Roberson <robersoncorreajr@gmail.com>
2025-02-21 14:45:09 -0300

    Montando uma página de carros

---
+++ 20-02/js/carros.js
@@ -0,0 +1,18 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "Corola",
+        year: 2022
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2008
+    },
+];
+
+console.log(cars [0].model);
+//Ano do honda
+console.log(cars[1].year)
+//Ano do Toyota
+console.log(cars[0].year)
