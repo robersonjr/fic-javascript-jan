aa8a55a2b8d82dac20aadf63192ffa247f2cb97b
Roberson <robersoncorreajr@gmail.com>
2025-02-25 15:59:49 -0300

    adicionando mais coisas ao react

--- REACT/primeiro-projeto/src/App.jsx
+++ REACT/primeiro-projeto/src/App.jsx
@@ -5,9 +5,16 @@ function App() {
 
   return (
     <>
+    <div className="container">
+      <header>
+        <h1> Logo </h1>
+      </header>
+      <h1>Seja bem vindo</h1>
+
       <p>
-        Roberson
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis accusantium officiis labore odit vitae? Quo esse et, ipsum consequuntur vitae saepe cumque dolor velit modi quia sed explicabo nam. Aut.
       </p>
+    </div>
     </>
   )
 }
