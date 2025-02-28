ddc16d5242bbe3e2308f8ad900a2e8e04732eb20
Roberson <robersoncorreajr@gmail.com>
2025-02-28 14:40:33 -0300

    fazendo meu primeiro componente

--- REACT/primeiro-projeto/src/App.jsx
+++ REACT/primeiro-projeto/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import ImgCard from "./Components/ImgCard";
 import Card from "./Components/Card";
+import Text from "./Components/Text";
 import img1 from "./assets/img-1.jpg"
 import img2 from "./assets/img-2.jpg"
 import img3 from "./assets/img-3.jpg"
@@ -21,6 +22,7 @@ function App() {
         <h1>Bem vindo ao meu site</h1>
         <p>Aqui você encontra as melhores oportunidades.</p>
       </Banner>
+       <Text />
 
       <div>
       <ImgCard caption= "Uma imagem legal" newImg={img1} />
---
+++ REACT/primeiro-projeto/src/Components/Text/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+export default function FirstComponent(props) {
+  return (
+    <div className="text-container">
+        <p className="text-container"><b>JR na Área</b></p>
+    </div>
+  )
+}
